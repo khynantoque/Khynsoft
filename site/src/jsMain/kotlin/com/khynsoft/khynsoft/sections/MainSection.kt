3f750a9509d71a12b3f77d105c292e7c435e4fd0
Khyn Antoque <khynantoque@gmail.com>
2023-07-17 01:58:00 +0800

    Section Test

--- site/src/jsMain/kotlin/com/khynsoft/khynsoft/sections/MainSection.kt
+++ site/src/jsMain/kotlin/com/khynsoft/khynsoft/sections/MainSection.kt
@@ -1,7 +1,6 @@
 package com.khynsoft.khynsoft.sections
 
 import androidx.compose.runtime.Composable
-import com.khynsoft.khynsoft.util.Constants
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
@@ -10,7 +9,7 @@ import org.jetbrains.compose.web.css.px
 @Composable
 fun MainSection() {
     Box(
-        modifier = Modifier.maxWidth(Constants.SECTION_WIDTH.px)
+        modifier = Modifier.maxWidth(1920.px)
     ) {
         MainBackground()
     }
