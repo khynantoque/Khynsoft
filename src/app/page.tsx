2cface6628c5fce156baf7ada8d5a8b9c401349f
Khyn Antoque <khynantoque@gmail.com>
2024-03-23 00:42:13 +0800

    changed hero text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,32 +30,36 @@ const HeroPage = () => {
       <TypewriterEffectSmooth
         words={[
           {
+            text: "We",
+          },
+          {
             text: "Build",
           },
           {
-            text: "an",
+            text: "IT,",
+            className: "new"
           },
           {
-            text: "awesome",
+            text: "To",
           },
           {
-            text: "software",
+            text: "Build",
           },
           {
-            text: "with",
+            text: "your",
           },
           {
-            text: "Khynsoft.",
+            text: "Trust.",
             className: "text-blue-500 dark:text-blue-500",
           },
         ]}
       />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
         <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
-          Join now
+          About Us
         </button>
         <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
-          Signup
+          Previous Work
         </button>
       </div>
     </div>
